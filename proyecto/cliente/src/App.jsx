bff15af5e68974f967732574c73fe7791db70621
nicolasmnz <nicolas.munozra@usm.cl>
2025-05-18 02:08:41 -0400

    Pagina añadida para ahcer un "ensayo" Faltan hacer pequeñas correcciones de estilo y revisar el docker/makefile

--- proyecto/cliente/src/App.jsx
+++ proyecto/cliente/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import BancoPreguntas from './pages/BancoPreguntas'
-import CreadorPreguntas from './pages/CreadorPreguntas'
-import Home from './pages/Home'
+
 import Landing from './pages/Landing'
+import Home from './pages/Home'
+import CreadorPreguntas from './pages/CreadorPreguntas'
+import BancoPreguntas from './pages/BancoPreguntas'
+import Ensayos from './pages/Ensayos'
+import Revisar from './pages/Revisar'
+
 
 export default function App() {
   return (
     <BrowserRouter>
         <Routes>
+            <Route path = '/' element = {<Home/>}/>  {/*este sera el root*/}
             <Route path = '/bancopreguntas' element = {<BancoPreguntas/>}/> 
             <Route path = '/creadorpreguntas' element = {<CreadorPreguntas/>}/> 
             <Route path = '/landing' element = {<Landing/>}/>  {/* Para crear el inicio de sesión*/}
-            <Route path = '/' element = {<Home/>}/>  {/*este sera el root*/}
+            <Route path = '/ensayo/:id' element = {<Ensayos/>}/> {/*arreglar para agregar id-> segun ensayo en base de datos*/}
+            <Route path = '/revisar/:id' element = {<Revisar/>}/>
         </Routes>
     </BrowserRouter>
   )
--- proyecto/cliente/src/components/Header.jsx
+++ proyecto/cliente/src/components/Header.jsx
@@ -1,33 +1,40 @@
-import logo from "../assets/img/logo150.webp"
 import { useNavigate } from "react-router-dom"
+
+import logo from "../assets/img/logo150.webp"
 
 export default function Header() {
   const navigate = useNavigate()
 
   return (
     <header>
-      <div className = "pedro">
-          <img src = {logo} style={{ marginRight: "auto", width: "80px", height: "80px"}} onClick={() => navigate("/")}/>
-          <button>
-            Usuario
-          </button>
-          <button>
-            Cerrar sesión
-          </button>
+      <div className="pedro">
+        <img src={logo} style={{ marginRight: "auto", width: "80px", height: "80px" }} onClick={() => navigate("/")} />
+        <button>
+          Usuario
+        </button>
+        <button>
+          Cerrar sesión
+        </button>
       </div>
       <nav>
         <button
-            onClick={() => navigate("/bancopreguntas")}
-            activate = "arreglar despues"
-            >
-            Banco de Preguntas
-          </button>
-          <button
-            onClick={() => navigate("/creadorpreguntas")}
-            activate = "arreglar despues"
-          >
-            Creador de Preguntas
-          </button>
+          onClick={() => navigate("/bancopreguntas")}
+          activate="arreglar despues"
+        >
+          Banco de Preguntas
+        </button>
+        <button
+          onClick={() => navigate("/creadorpreguntas")}
+          activate="arreglar despues"
+        >
+          Creador de Preguntas
+        </button>
+        <button
+          onClick={() => navigate("/ensayo/:id")}
+          activate="arreglar despues"
+        >
+          Ensayos
+        </button>
       </nav>
     </header>
   )
