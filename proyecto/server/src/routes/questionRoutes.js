d840ed7616e2c4fcfcc61457f4e4ec4708c123fc
SEBINHOCL <114964485+SEBINHOCL@users.noreply.github.com>
2025-09-11 13:05:39 -0600

    Delete proyecto directory

--- proyecto/server/src/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getQuestions, createQuestion } from '../controllers/questionController.js';
-
-const router = express.Router();
-
-router.get('/', getQuestions);
-router.post('/', createQuestion);
-
-export default router;
--- proyecto/server/src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createTest, getTests } from '../controllers/testController.js';
-
-const router = express.Router();
-
-router.get('/', getTests);
-router.post('/', createTest);
-
-export default router;
