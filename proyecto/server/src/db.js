88d471fc2581fdb933ccd6706188b37e89d545fd
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-10-09 18:57:41 -0300

    feat: conexion a la bbdd desde la api ocupa variables de entorno correctamente
    también mejoraron los comentarios del .env.example

--- proyecto/server/src/db.js
+++ proyecto/server/src/db.js
@@ -3,8 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const dbConfig = {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASSWORD,
+  hostname: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  name: process.env.DB_NAME
+};
+
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@db:5432/myapp',
+  `postgres://${dbConfig.user}:${dbConfig.pass}@${dbConfig.hostname}:${dbConfig.port}/${dbConfig.name}`,
   {
     dialect: 'postgres',
     logging: false
