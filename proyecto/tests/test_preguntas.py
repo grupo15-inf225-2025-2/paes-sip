49bfdff61da5a80c2ba930ade0fa1fdfde4e43cc
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-10-13 23:34:29 -0300

    feat(tests):tests para el hito 3

--- proyecto/tests/test_preguntas.py
+++ proyecto/tests/test_preguntas.py
@@ -5,6 +5,28 @@ class TestEndpointPreguntas(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		cls.endpointUrl = "http://backend:3001/api/pregunta"
-	def test_get_vacio(self):
+		cls.pregunta_valida = {
+			"asignatura": "Ciencia y Tecnología",
+            "tematica": "Física Cuántica",
+            "habilidad": "Conceptualización",
+            "pregunta": "Pregunta de prueba única",
+            "opciones": {"a": "Opción X", "b": "Opción Y"},
+            "correcta": 1,
+            "puntos": 5,
+			"libre": True
+        }
+	def test_get(self):
 		response = requests.get(self.endpointUrl)
 		self.assertEqual(response.status_code, 200)
+
+		data = response.json()
+		self.assertIsInstance(data, list)  # Verifica que es una lista
+
+	def test_post_valido(self):
+		response = requests.post(self.endpointUrl, json=self.pregunta_valida)
+		self.assertEqual(response.status_code, 201)
+
+		data = response.json()
+		self.assertIn("id", data) # Verifica que se haya asignado un ID
+		# cleanup
+		requests.delete(f"{self.endpointUrl}/{data['id']}")
