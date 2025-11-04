c848a05ab19b3695ae312c2115e3e802e8d20891
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-11-03 23:29:30 -0300

    eliminar referencias a resultado
    se desarrollará posteriormente

--- proyecto/server/src/models/index.js
+++ proyecto/server/src/models/index.js
@@ -2,22 +2,8 @@ import sequelize from '../db.js';
 import { DataTypes } from 'sequelize';
 import PreguntaModel from './questionModel.js';
 import EnsayoModel from './testModel.js';
-import ResultadoModel from './resultModel.js';
 
 const Pregunta = PreguntaModel(sequelize, DataTypes);
 const Ensayo = EnsayoModel(sequelize, DataTypes);
-const Resultado = ResultadoModel(sequelize, DataTypes);
 
-
-Ensayo.hasMany(Resultado, {
-  foreignKey: 'ensayoId',
-  as: 'resultados',
-});
-
-Resultado.belongsTo(Ensayo, {
-  foreignKey: 'ensayoId',
-  as: 'ensayo',
-});
-
-
-export { sequelize, Pregunta, Ensayo, Resultado };
+export { sequelize, Pregunta, Ensayo };
